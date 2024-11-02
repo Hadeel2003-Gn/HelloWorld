01020cd9d149a3c77441274a4c80e416fb207242
Hadeel2003-Gn <gounihadilasma@gmail.com>
2024-11-02 19:44:29 +0100

    Update: Added additional console message to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,6 @@
-public class HelloWorld {      public static void main(String[] args) {          System.out.println("Hello, World!");      }  } 
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Message original
+        System.out.println("Je suis une version modifiée du code de l'exo 1"); // Message modifié
+    }
+}
